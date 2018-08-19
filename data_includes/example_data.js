705ea49af08ce572432f893eb35ea88580b82f47
PennController <39499242+PennController@users.noreply.github.com>
2018-08-18 21:29:55 -0400

    newText in before (beta 0.3)

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -4,7 +4,7 @@ PennController.ResetPrefix(null);
 var items = [
     ["trial", "PennController", PennController(
         newTextInput("alternative")
-            .settings.before("I thought the weather was warm, but it really is ")
+            .settings.before( newText("before", "I thought the weather was warm, but it really is ") )
             .print()
         ,
         newButton("validation", "Click here to continue.")
